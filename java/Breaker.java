0d2e87cbe3983d553e5990be5f0b4f8fbdc90e79
mazuschlag <markazuschlag@gmail.com>
2018-02-22 18:36:14 -0800

    Works with single character brute force

--- java/Breaker.java
+++ java/Breaker.java
@@ -1,26 +1,35 @@
 // Breaker.java
 import java.io.*;
+import java.math.*;
 import java.lang.Thread;
 import java.lang.Runtime;
 import java.util.Hashtable;
 import java.util.ArrayList;
 import java.util.Queue;
 import java.util.LinkedList;
-import java.math.BigInteger;
 import java.util.Set;
 
 public class Breaker {
 	public class BreakerThread implements Runnable {
-		Queue<String> keys;
-		Hashtable<String,String> keyHashes;
-		ArrayList<String> foundPasswords;
-		boolean finished = false;
-		int i = 0;
+		private Queue<String> keys;
+		private Hashtable<String,String> keyHashes;
+		private final int TOTAL_THREADS;
+		private int threadsRunning;
+		private int i, j, k, l, m, n, o = 0;
+		private static final int NULL_FLAG = -1;
+		private static final int I_FLAG = 0;
+		private static final int J_FLAG = 1;
+		private static final int K_FLAG = 2;
+		private static final int L_FLAG = 3;
+		private static final int M_FLAG = 4;
+		private static final int N_FLAG = 5;
+		private static final int O_FLAG = 6;
 
-		public BreakerThread(Queue<String> keyBuffer, Hashtable<String,String> hashTable) {
+		public BreakerThread(Queue<String> keyBuffer, Hashtable<String,String> hashTable, int threads) {
 			keys = keyBuffer;
 			keyHashes = hashTable;
-			foundPasswords = new ArrayList<String>();
+			TOTAL_THREADS = threads;
+			threadsRunning = threads;
 		}
 
 		public void run() {
@@ -32,21 +41,121 @@ public class Breaker {
 			}
 			//System.out.println("Checking alphabet");
 			while (i < ALPHABET.length) {
-				String current = Character.toString(ALPHABET[i]);
+				String current = getStringFromIncrement(I_FLAG, i);
 				String result = fnv(current);
 				checkResult(current, result);
-				i++;
+			}
+
+			waitForOthers(NULL_FLAG, I_FLAG);
+			
+			while (i < ALPHABET.length) {
+				while (j < ALPHABET.length) {
+					String current = getStringFromIncrement();
+					StringBuilder current = new StringBuilder().append(ALPHABET[i]).append(ALPHABET[j]);
+					increment(J_FLAG);
+					String result = fnv(current.toString());
+					checkResult(current.toString(), result);
+				}
+				waitForOthers(I_FLAG, J_FLAG);
 			}
 		}
 
-		public synchronized String popFromQueue() {
+		private synchronized void waitForOthers(int incrementFlag, int... resetFlags) {
+			threadsRunning--;
+			while (threadsRunning > 0) {	
+				//System.out.println("Thread " + Thread.currentThread().getName() + " waiting...");
+				try {
+					wait();
+					//System.out.println("Thread " + Thread.currentThread().getName() + " is awake!");
+				} catch (InterruptedException e) {
+					//System.out.println("Thread " + Thread.currentThread().getName() + " interrupted!");
+				}
+				break;
+			}
+			if (threadsRunning == 0) {
+				//System.out.println("Thread " + Thread.currentThread().getName() + " will notify!");
+				threadsRunning = TOTAL_THREADS;
+				for (int flag : resetFlags) {
+					resetIncrement(flag);
+				}
+				increment(incrementFlag);
+				notifyAll();
+			}
+		}
+
+		private synchronized String getStringFromIncrement(int flag, int index) {
+			if (index < ALPHABET.length) {
+				increment(I_FLAG);
+				return Character.toString(ALPHABET[index]);
+			}
+			return "";
+		}
+
+		private void increment(int flag) {
+			switch(flag) {
+				case I_FLAG:
+					i++;
+					break;
+				case J_FLAG:
+					j++;
+					break;
+				case K_FLAG:
+					k++;
+					break;
+				case L_FLAG:
+					l++;
+					break;
+				case M_FLAG:
+					m++;
+					break;
+				case N_FLAG:
+					n++;
+					break;
+				case O_FLAG:
+					o++;
+					break;
+				default:
+					break;
+			}
+		}
+
+		private void resetIncrement(int flag) {
+			switch(flag) {
+				case I_FLAG:
+					i = 0;
+					break;
+				case J_FLAG:
+					j = 0;
+					break;
+				case K_FLAG:
+					k = 0;
+					break;
+				case L_FLAG:
+					l = 0;
+					break;
+				case M_FLAG:
+					m = 0;
+					break;
+				case N_FLAG:
+					n = 0;
+					break;
+				case O_FLAG:
+					o = 0;
+					break;
+				default:
+					break;
+			}
+		}
+
+		private synchronized String popFromQueue() {
 			if (!keys.isEmpty()) {
 				return keys.remove();	
 			}
 			return "";
 		}
 
-		public synchronized String fnv(String byteString) {
+
+		private synchronized String fnv(String byteString) {
 			if (byteString != "") {
 				byte[] bytes = byteString.getBytes();
 				BigInteger hash = Breaker.FNV_OFFSET;
@@ -60,13 +169,13 @@ public class Breaker {
 			return "";
 		}
 
-		public synchronized void checkResult(String current, String result) {
+		private synchronized void checkResult(String current, String result) {
 			if (keyHashes.get(result) != null) {
 				keyHashes.put(result, current);
 			}
 		} 
 
-		public void printResults() {
+		private void printResults() {
 			//System.out.println("Total output: " + keyHashes.size());
 			StringBuffer stringBuffer = new StringBuffer();
 			Set<String> keys = keyHashes.keySet();
@@ -79,7 +188,7 @@ public class Breaker {
 			System.out.print(stringBuffer);
 		}
 
-		public void checkMissing(Hashtable<String, String> input) {
+		private void checkMissing(Hashtable<String, String> input) {
 		 	Set<String> inKeys = input.keySet();
 		 	for (String inKey : inKeys) {
 		 		if (keyHashes.get(inKey) == null) {
@@ -139,7 +248,7 @@ public class Breaker {
 
 	public void runThreads() {
 		int bufferSize = hashTable.size();
-		BreakerThread breakerThread = new BreakerThread(keyBuffer, hashTable);
+		BreakerThread breakerThread = new BreakerThread(keyBuffer, hashTable, CORES);
 		Thread[] threadList = new Thread[CORES];
 		for (int i = 0; i < threadList.length; i++) {
 			threadList[i] = new Thread(breakerThread, Integer.toString(i));
